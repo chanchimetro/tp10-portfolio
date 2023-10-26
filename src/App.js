7fcaadc06a302b17d443ebfe0d7bc815c7a9f10f
IGNACIO MATIAS LOPEZ ABRAHAM <46872351@ort>
2023-10-26 10:37:53 -0300

    fixed links list

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './pages/Layout';
 import Home from './pages/Home';
 import AboutMe from './pages/AboutMe';
+import Works from './pages/Works';
+import Favourites from './pages/Favourites';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Route path='/' element={<Layout/>}>
         <Route index element={<Home/>}/>
         <Route path='AboutMe' element={<AboutMe/>}/>
+        <Route path='Works' element={<Works/>}/>
+        <Route path='Favourites' element={<Favourites/>}/>
       </Route>
     </Routes>
     </BrowserRouter>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,32 @@
 import { Grid } from "@mui/material";
+import { Link } from "react-router-dom";
 import computerOutline from "../assets/computer-outline-right.png"
 import React from "react";
 import "../styles/Home.css";
 
 function Home() {
 	return (
-		<Grid className="mainMenu" container spacing={2}>
-			<Grid item xs={2}>
-				<img className="homeImg" src={computerOutline}/>
+		<Grid className="mainMenu" container>
+			<Grid item xs={6}>
+				<p className="img-holder">
+					<img className="homeImg" src={computerOutline} />
+				</p>
+			</Grid>
+			<Grid item xs={6}>
+				<ul className="links">
+					<li>
+						<Link>PROJECTS</Link>
+					</li>
+					<li>
+						<Link>FAVOURITES</Link>
+					</li>
+					<li>
+						<Link>ABOUT ME</Link>
+					</li>
+					<li>
+						<Link>HOLA</Link>
+					</li>
+				</ul>
 			</Grid>
 		</Grid>
 	);
