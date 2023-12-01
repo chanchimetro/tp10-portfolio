c07865f67cfaaf3ad90d19691b02da8cecd6e4bf
chanchimetro <48773442+chanchimetro@users.noreply.github.com>
2023-12-01 06:38:46 -0300

    fix

--- src/pages/Works.js
+++ src/pages/Works.js
@@ -1,14 +1,22 @@
 import "../styles/Gallery.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import projectsContext from "../context/projectsContext";
 import ProjectGallery from "../components/js/ProjectGallery";
 import { useContext } from "react";
 import { Grid } from "@mui/material";
 
 function Works() {
+	const [loading, setLoading] = useState(true);
 	const pContext = useContext(projectsContext);
 
+	useEffect(() => {
+		if (pContext.projects) {
+			setLoading(false);
+		}
+	},[pContext])
+
 	return (
+		loading ? <></> :
 		<>
 			<Grid className="galleryCont">
 				<h1 className="galleryTitle">
